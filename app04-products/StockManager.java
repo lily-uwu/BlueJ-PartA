7af79efa1cea8f5961bd9c0263145d4058202a12
lily-uwu <lilymcculloughuwu@gmail.com>
2020-11-08 18:50:16 +0000

    Added description - StockManager
    -added a description to the StockManager class
    -removed broken methods

--- app04-products/StockManager.java
+++ app04-products/StockManager.java
@@ -1,16 +1,17 @@
 import java.util.ArrayList;
 
 /**
- * Manage the stock in a business.
- * The stock is described by zero or more Products.
+ * Simulates a stock management system where a user can add new products to their stock list
+ * and make changes to these products such as quantity, name etc.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Lily Mccullough
+ * @version 03/11/2020
  */
 public class StockManager
 {
-    // A list of the products.
+    // Creates an array to contain products
     private ArrayList<Product> stock;
+    private Product product;
 
     /**
      * Initialise the stock manager.
@@ -18,53 +19,5 @@ public class StockManager
     public StockManager()
     {
         stock = new ArrayList<>();
-    }
-
-    /**
-     * Add a product to the list.
-     * @param item The item to be added.
-     */
-    public void addProduct(Product item)
-    {
-        stock.add(item);
-    }
-    
-    /**
-     * Receive a delivery of a particular product.
-     * Increase the quantity of the product by the given amount.
-     * @param id The ID of the product.
-     * @param amount The amount to increase the quantity by.
-     */
-    public void delivery(int id, int amount)
-    {
-    }
-    
-    /**
-     * Try to find a product in the stock with the given id.
-     * @return The identified product, or null if there is none
-     *         with a matching ID.
-     */
-    public Product findProduct(int id)
-    {
-        return null;
-    }
-    
-    /**
-     * Locate a product with the given ID, and return how
-     * many of this item are in stock. If the ID does not
-     * match any product, return zero.
-     * @param id The ID of the product.
-     * @return The quantity of the given product in stock.
-     */
-    public int numberInStock(int id)
-    {
-        return 0;
-    }
-
-    /**
-     * Print details of all the products.
-     */
-    public void printProductDetails()
-    {
     }
 }
