c25102b66515e2153065b963190d2773c411b583
lily-uwu <lilymcculloughuwu@gmail.com>
2021-01-13 00:21:05 +0000

    added player inventory
    added an arraylist to store items for the player

--- app06-zuul-game/app06-zuul-game/src/Player.java
+++ app06-zuul-game/app06-zuul-game/src/Player.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
+
 /**
  * This class represents the player and contains the players inventory
  */
 public class Player
 {
-    public Player()
-    {
+    private int energy;
+    private ArrayList<Items> playerInv;
 
+    public Player(int energy)
+    {
+        this.energy = energy;
+        playerInv = new ArrayList<Items>();
     }
 
+    public int getEnergy()
+    {
+        return energy;
+    }
 }
